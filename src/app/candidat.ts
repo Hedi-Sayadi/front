cd815df1916be095fdbb2d613d55bd59e4408342
SayadiHedii <hedi.sayadi@supcom.tn>
2022-07-31 23:07:28 +0100

    first commit

---
+++ src/app/candidat.ts
@@ -0,0 +1,31 @@
+export class Candidat {
+    id: number =0;
+    name: string ="";
+    email: string="";
+    jobTitle: string="";
+    phone: string="";
+    imageUrl: string="";
+    candidatCode: string="";
+
+
+public Candidat (id:number,name: string ,email: string,jobTitle : string,phone : string, imageUrl: string,candidatCode: string){
+    this.id=id;
+    this.name=name;
+    this.email=email;
+    this.jobTitle=jobTitle;
+    this.phone=phone;
+    this.imageUrl;
+    this.candidatCode=candidatCode;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
