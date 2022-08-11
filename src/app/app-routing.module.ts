4bb8b14fdf8c9e0d41092f9d3e8e7ea415d22d43
SayadiHedii <hedi.sayadi@supcom.tn>
2022-08-11 09:13:47 +0100

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'SignUp' ,component:SignUpComponent},
+  {path:'SignIn' ,component:SignInComponent},
+  {path:'home' ,component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
